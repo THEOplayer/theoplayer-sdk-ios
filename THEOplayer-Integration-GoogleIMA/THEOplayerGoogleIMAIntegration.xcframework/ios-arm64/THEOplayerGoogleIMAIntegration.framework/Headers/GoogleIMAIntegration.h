d60b6bbd9aac962c9d5c9845c96c171d0fa381e1
git <THEObuildbot>
2023-04-05 14:37:10 +0200

    Release THEOplayerGoogleIMAIntegration version 5.0.0

---
+++ THEOplayer-Integration-GoogleIMA/THEOplayerGoogleIMAIntegration.xcframework/ios-arm64/THEOplayerGoogleIMAIntegration.framework/Headers/GoogleIMAIntegration.h
@@ -0,0 +1,18 @@
+//
+//  GoogleIMAIntegration.h
+//  GoogleIMAIntegration
+//
+//  Created by Raffi Manissalian on 01/09/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GoogleIMAIntegration.
+FOUNDATION_EXPORT double GoogleIMAIntegrationVersionNumber;
+
+//! Project version string for GoogleIMAIntegration.
+FOUNDATION_EXPORT const unsigned char GoogleIMAIntegrationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleIMAIntegration/PublicHeader.h>
+
+

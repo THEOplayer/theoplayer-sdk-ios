d070ab85613c988dd0d9fb78f834ebb5b602a746
git <THEObuildbot>
2023-04-05 14:58:22 +0200

    Release THEOplayerGoogleCastIntegration version 5.0.0

---
+++ THEOplayer-Integration-GoogleCast/THEOplayerGoogleCastIntegration.xcframework/ios-arm64/THEOplayerGoogleCastIntegration.framework/Headers/GoogleCastIntegration.h
@@ -0,0 +1,18 @@
+//
+//  GoogleCastIntegration.h
+//  GoogleCastIntegration
+//
+//  Created by danesz on 18/08/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GoogleCastIntegration.
+FOUNDATION_EXPORT double GoogleCastIntegrationVersionNumber;
+
+//! Project version string for GoogleCastIntegration.
+FOUNDATION_EXPORT const unsigned char GoogleCastIntegrationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleCastIntegration/PublicHeader.h>
+
+
